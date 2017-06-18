27cebae8c44a048da0e29cbe460062c461ae84a6
Arcanyx <nikhilchaughule@gmail.com>
2017-06-18 19:51:42 +0530

    7. Multiply 2 nos

---
+++ multwonumdyn.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class multwonumdyn{
+	public static void main(String args[]){
+		Scanner s = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter first number:");
+		a = s.nextInt();
+		System.out.println("Enter second number:");
+		b = s.nextInt();
+		c = b*a;
+		System.out.println("Product is "+c);
+	}
+}
