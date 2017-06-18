188ef8f6a38edb9bafdbfd14b8cefe914f53035d
Arcanyx <nikhilchaughule@gmail.com>
2017-06-18 19:54:25 +0530

    5 and 6

---
+++ subtwonumdyn.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class subtwonumdyn{
+	public static void main(String args[]){
+		Scanner s = new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter first number:");
+		a = s.nextInt();
+		System.out.println("Enter second number:");
+		b = s.nextInt();
+		c = b-a;
+		System.out.println("Difference is "+c);
+	}
+}
